693937abe3f1279d9964389427eb07a14452fac5
Nicholas Charriere <nicholascharriere@gmail.com>
2024-06-21 15:32:54 -0700

    Commit my changes

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  -- import/override with your plugins folder
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
