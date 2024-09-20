d13bf9d801c9e7b844eb82b2ba2fdec005dc12f9
Nicholas Charriere <nicholascharriere@gmail.com>
2024-09-20 10:49:21 +0200

    Add lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,19 @@
+return {
+  "kdheepak/lazygit.nvim",
+
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+    "LazyGitStage",
+    "LazyGitUnstage",
+  },
+
+  keys = {
+    "<Leader>lg",
+    "<cmd>LazyGit<CR>",
+    desc = "open LazyGit",
+  },
+}
